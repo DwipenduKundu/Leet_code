449eb3eb4ab4048a9b9fd5593f4ffe84a5f951d5
Dwipendu Kundu <97604818+DwipenduKundu@users.noreply.github.com>
2025-06-16 17:08:09 +0530

    Time: 46 ms (35.70%) | Memory: 53.6 MB (33.72%) - LeetSync

---
+++ JS/promises/2733-sleep/sleep.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+    let p1= new Promise(res=>{
+        setTimeout(res,millis);
+    })
+    return p1
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
